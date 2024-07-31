6042696e6b0c36e7672e10240b342462d7fd4dee
sherzod-uralov <sherzoduralov432@gmail.com>
2024-07-31 11:30:59 +0500

    vercel deploy

--- next.config.mjs
+++ next.config.mjs
@@ -4,13 +4,7 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: true,  // Fayllarning nomiga '/' qo'shadi
-    generateStaticParams() {
-        return [
-            { locales: 'en' },
-            { locales: 'uz' }
-        ];
-    }
+    trailingSlash: true,
 };
 
 export default withNextIntl(nextConfig);
